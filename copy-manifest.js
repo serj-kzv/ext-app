d49f82343c8aa307a5a6681144f4819fd414b197
null <null>
2020-09-27 12:12:42 +0300

    null

--- copy-manifest.js
+++ copy-manifest.js
@@ -2,10 +2,10 @@ const fs = require('fs-extra');
 
 async function copyFiles () {
     try {
-        await fs.copy('./manifest.json', './dist/manifest.json')
-        console.log('manifest.json is copied successfully!')
+        await fs.copy('./manifest.json', './dist/manifest.json');
+        console.log('manifest.json is copied successfully!');
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
 }
 
